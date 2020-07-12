3c864acaf86bf07c6ef57984905814305dd97a2d
Bora Yarkın <borayarkin02@gmail.com>
2020-07-13 02:30:56 +0300

    Projeler Oluşturuldu

---
+++ Eticaret/Eticaret.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eticaret.WebUI.Startup))]
+namespace Eticaret.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
