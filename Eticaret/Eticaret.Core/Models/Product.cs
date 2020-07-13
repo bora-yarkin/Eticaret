91a236c42d87a5d1e90474acb6f4397377e1670f
Bora Yarkın <borayarkin02@gmail.com>
2020-07-13 03:58:58 +0300

    Kategoriler, Ürün Yönetimi eklendi nuget paketleri güncellendi

---
+++ Eticaret/Eticaret.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Ürün Adı")]
+        public string Name { get; set; }
+        [DisplayName("Açıklama")]
+        public string Description { get; set; }
+        [DisplayName("Fiyat")]
+        public decimal Price { get; set; }
+        [DisplayName("Kategori")]
+        public string Category { get; set; }
+        [DisplayName("Resim")]
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Eticaret/Eticaret.Core/Models/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret.Core.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+        [DisplayName("Kategori")]
+        public string Category { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
