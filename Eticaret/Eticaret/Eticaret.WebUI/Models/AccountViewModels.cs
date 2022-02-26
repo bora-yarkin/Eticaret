a2aaaa8a9a0aacfdafe1e849b7d91ac31af1734a
Bora Yarkın <borayarkin02@gmail.com>
2022-02-26 21:04:16 +0300

    .

---
+++ Eticaret/Eticaret/Eticaret.WebUI/Models/AccountViewModels.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eticaret.WebUI.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Kod")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Bu tarayıcıyı hatırla")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} Şifreler en az {2} uzunluğunda olmalıdır.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre Tekrarı")]
+        [Compare("Password", ErrorMessage = "Şifreler Uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DisplayName("Ad")]
+        public string FirstName { get; set; }
+        [DisplayName("Soyad")]
+        [Required]
+        public string LastName { get; set; }
+        [DisplayName("Sokak")]
+        [Required]
+        public string Street { get; set; }
+        [DisplayName("Şehir")]
+        [Required]
+        public string City { get; set; }
+        [DisplayName("Mahalle")]
+        public string State { get; set; }
+        [DisplayName("Zip Kodu")]
+        [Required]
+        public string ZipCode { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} Şifreler en az {2} uzunluğunda olmalıdır.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifreyi Doğrula")]
+        [Compare("Şifre", ErrorMessage = "Şifreler Uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
